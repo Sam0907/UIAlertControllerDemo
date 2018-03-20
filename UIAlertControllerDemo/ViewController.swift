f32b0ebb1aefbdf7dd85394d17fdff9c0a7b78bc
gdcp <gdcp@m44.local>
2018-03-20 10:39:41 +0800

    Initial Commit

---
+++ UIAlertControllerDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  UIAlertControllerDemo
+//
+//  Created by gdcp on 2018/3/20.
+//  Copyright © 2018年 gdcp. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
